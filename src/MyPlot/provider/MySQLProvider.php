0aa73dfc2e0523d9f532b6618b77e86c9d99b31e
jasonwynn10 <jasonwynn10@me.com>
2020-04-06 01:44:01 -0400

    Start testing Asynql provider support

--- src/MyPlot/provider/MySQLProvider.php
+++ src/MyPlot/provider/MySQLProvider.php
@@ -4,31 +4,25 @@ namespace MyPlot\provider;
 
 use MyPlot\MyPlot;
 use MyPlot\Plot;
-use pocketmine\Server;
+use poggit\libasynql\base\DataConnectorImpl;
+use poggit\libasynql\libasynql;
 
 class MySQLProvider extends DataProvider {
 	/** @var MyPlot $plugin */
 	protected $plugin;
-	/** @var \mysqli $db */
-	protected $db;
+	/** @var DataConnectorImpl $db */
+	private $db;
 	/** @var array $settings */
-	protected $settings;
-	/** @var \mysqli_stmt $sqlGetPlot */
-	protected $sqlGetPlot;
-	/** @var \mysqli_stmt $sqlSavePlot */
-	protected $sqlSavePlot;
-	/** @var \mysqli_stmt $sqlSavePlotById */
-	protected $sqlSavePlotById;
-	/** @var \mysqli_stmt $sqlRemovePlot */
-	protected $sqlRemovePlot;
-	/** @var \mysqli_stmt $sqlRemovePlotById */
-	protected $sqlRemovePlotById;
-	/** @var \mysqli_stmt $sqlGetPlotsByOwner */
-	protected $sqlGetPlotsByOwner;
-	/** @var \mysqli_stmt $sqlGetPlotsByOwnerAndLevel */
-	protected $sqlGetPlotsByOwnerAndLevel;
-	/** @var \mysqli_stmt $sqlGetExistingXZ */
-	protected $sqlGetExistingXZ;
+	private $settings;
+	/** @var string */
+	private $sqlGetPlot = "SELECT id, name, owner, helpers, denied, biome, pvp FROM plots WHERE level = ? AND X = ? AND Z = ?;",
+		$sqlSavePlot = "INSERT INTO plots (`id`, `level`, `X`, `Z`, `name`, `owner`, `helpers`, `denied`, `biome`, `pvp`) VALUES((SELECT id FROM plots p WHERE p.level = ? AND X = ? AND Z = ?),?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE name = VALUES(name), owner = VALUES(owner), helpers = VALUES(helpers), denied = VALUES(denied), biome = VALUES(biome), pvp = VALUES(pvp);",
+		$sqlSavePlotById = "UPDATE plots SET id = ?, level = ?, X = ?, Z = ?, name = ?, owner = ?, helpers = ?, denied = ?, biome = ?, pvp = ? WHERE id = VALUES(id);",
+		$sqlRemovePlot = "DELETE FROM plots WHERE id = ?;",
+		$sqlRemovePlotById = "DELETE FROM plots WHERE level = ? AND X = ? AND Z = ?;",
+		$sqlGetPlotsByOwner = "SELECT * FROM plots WHERE owner = ?;",
+		$sqlGetPlotsByOwnerAndLevel = "SELECT * FROM plots WHERE owner = ? AND level = ?;",
+		$sqlGetExistingXZ = "SELECT X, Z FROM plots WHERE (level = ? AND ((abs(X) = ? AND abs(Z) <= ?) OR (abs(Z) = ? AND abs(X) <= ?)));";
 
 	/**
 	 * MySQLProvider constructor.
@@ -45,16 +39,21 @@ class MySQLProvider extends DataProvider {
 		$this->plugin = $plugin;
 		parent::__construct($plugin, $cacheSize);
 		$this->settings = $settings;
-		$this->db = new \mysqli($settings['Host'], $settings['Username'], $settings['Password'], $settings['DatabaseName'], $settings['Port']);
-		if($this->db->connect_error)
-			throw new \RuntimeException("Failed to connect to the MySQL database: " . $this->db->connect_error);
-		$this->db->query("CREATE TABLE IF NOT EXISTS plots (id INT PRIMARY KEY AUTO_INCREMENT, level TEXT, X INT, Z INT, name TEXT, owner TEXT, helpers TEXT, denied TEXT, biome TEXT, pvp INT);");
-		try{
-			$this->db->query("ALTER TABLE plots ADD COLUMN pvp INT AFTER biome;");
-		}catch(\Exception $e) {
-			// do nothing :P
-		}
-		$this->prepare();
+		$this->db = libasynql::create($plugin, [
+			"type" => "mysql",
+			"mysql" => [
+				"host" => $settings['Host'],
+				"username" => $settings['Username'],
+				"password" => $settings['Password'],
+				"schema" => $settings['DatabaseName'],
+				"port" => $settings['Port']
+			],
+			"worker-limit" => 2
+		], [
+			"mysql" => []
+		]);
+		$this->db->executeGenericRaw("CREATE TABLE IF NOT EXISTS plots (id INT PRIMARY KEY AUTO_INCREMENT, level TEXT, X INT, Z INT, name TEXT, owner TEXT, helpers TEXT, denied TEXT, biome TEXT, pvp INT);");
+		$this->db->executeGenericRaw("ALTER TABLE plots ADD COLUMN pvp INT AFTER biome;");
 		$this->plugin->getLogger()->debug("MySQL data provider registered");
 	}
 
@@ -64,20 +63,12 @@ class MySQLProvider extends DataProvider {
 	 * @return bool
 	 */
 	public function savePlot(Plot $plot) : bool {
-		$this->reconnect();
 		$helpers = implode(',', $plot->helpers);
 		$denied = implode(',', $plot->denied);
 		if($plot->id >= 0) {
-			$stmt = $this->sqlSavePlotById;
-			$stmt->bind_param('isiisssssi', $plot->id, $plot->levelName, $plot->X, $plot->Z, $plot->name, $plot->owner, $helpers, $denied, $plot->biome, $plot->pvp);
+			$this->db->executeChangeRaw($this->sqlSavePlotById, [$plot->id, $plot->levelName, $plot->X, $plot->Z, $plot->name, $plot->owner, $helpers, $denied, $plot->biome, $plot->pvp]);
 		}else{
-			$stmt = $this->sqlSavePlot;
-			$stmt->bind_param('siisiisssssi', $plot->levelName, $plot->X, $plot->Z, $plot->levelName, $plot->X, $plot->Z, $plot->name, $plot->owner, $helpers, $denied, $plot->biome, $plot->pvp);
-		}
-		$result = $stmt->execute();
-		if($result === false) {
-			$this->plugin->getLogger()->error($stmt->error);
-			return false;
+			$this->db->executeInsertRaw($this->sqlSavePlot, [$plot->levelName, $plot->X, $plot->Z, $plot->levelName, $plot->X, $plot->Z, $plot->name, $plot->owner, $helpers, $denied, $plot->biome, $plot->pvp]);
 		}
 		$this->cachePlot($plot);
 		return true;
@@ -89,18 +80,10 @@ class MySQLProvider extends DataProvider {
 	 * @return bool
 	 */
 	public function deletePlot(Plot $plot) : bool {
-		$this->reconnect();
 		if($plot->id >= 0) {
-			$stmt = $this->sqlRemovePlotById;
-			$stmt->bind_param('i', $plot->id);
+			$this->db->executeChangeRaw($this->sqlRemovePlot, [$plot->id]);
 		}else{
-			$stmt = $this->sqlRemovePlot;
-			$stmt->bind_param('sii', $plot->levelName, $plot->X, $plot->Z);
-		}
-		$result = $stmt->execute();
-		if($result === false) {
-			$this->plugin->getLogger()->error($stmt->error);
-			return false;
+			$this->db->executeChangeRaw($this->sqlRemovePlotById, [$plot->levelName, $plot->X, $plot->Z]);
 		}
 		$plot = new Plot($plot->levelName, $plot->X, $plot->Z);
 		$this->cachePlot($plot);
@@ -115,19 +98,10 @@ class MySQLProvider extends DataProvider {
 	 * @return Plot
 	 */
 	public function getPlot(string $levelName, int $X, int $Z) : Plot {
-		$this->reconnect();
 		if(($plot = $this->getPlotFromCache($levelName, $X, $Z)) != null) {
 			return $plot;
 		}
-		$stmt = $this->sqlGetPlot;
-		$stmt->bind_param('sii', $levelName, $X, $Z);
-		$result = $stmt->execute();
-		if($result === false) {
-			$this->plugin->getLogger()->error($stmt->error);
-			return new Plot($levelName, $X, $Z);
-		}
-		$result = $stmt->get_result();
-		if($val = $result->fetch_array(MYSQLI_ASSOC)) {
+		$this->db->executeSelectRaw($this->sqlGetPlot, [$levelName, $X, $Z], function($val, $columns) use ($levelName, $X, $Z, &$plot){
 			if(empty($val["helpers"])) {
 				$helpers = [];
 			}else{
@@ -140,9 +114,9 @@ class MySQLProvider extends DataProvider {
 			}
 			$pvp = is_numeric($val["pvp"]) ? (bool)$val["pvp"] : null;
 			$plot = new Plot($levelName, $X, $Z, (string) $val["name"], (string) $val["owner"], $helpers, $denied, (string) $val["biome"], $pvp, (int) $val["id"]);
-		}else{
-			$plot = new Plot($levelName, $X, $Z);
-		}
+			$this->cachePlot($plot);
+		});
+		$plot = new Plot($levelName, $X, $Z);
 		$this->cachePlot($plot);
 		return $plot;
 	}
@@ -154,35 +128,26 @@ class MySQLProvider extends DataProvider {
 	 * @return array
 	 */
 	public function getPlotsByOwner(string $owner, string $levelName = "") : array {
-		$this->reconnect();
-		if(empty($levelName)) {
-			$stmt = $this->sqlGetPlotsByOwner;
-			$stmt->bind_param('s', $owner);
-		}else{
-			$stmt = $this->sqlGetPlotsByOwnerAndLevel;
-			$stmt->bind_param('ss', $owner, $levelName);
-		}
 		$plots = [];
-		$result = $stmt->execute();
-		if($result === false) {
-			$this->plugin->getLogger()->error($stmt->error);
-			return $plots;
-		}
-		$result = $stmt->get_result();
-		while($val = $result->fetch_array()) {
+		$func = function($val, $columns) use (&$plots) {
 			$helpers = explode(",", (string) $val["helpers"]);
 			$denied = explode(",", (string) $val["denied"]);
 			$pvp = is_numeric($val["pvp"]) ? (bool)$val["pvp"] : null;
 			$plots[] = new Plot((string) $val["level"], (int) $val["X"], (int) $val["Z"], (string) $val["name"], (string) $val["owner"], $helpers, $denied, (string) $val["biome"], $pvp, (int) $val["id"]);
+
+			$plots = array_filter($plots, function($plot) {
+				return $this->plugin->isLevelLoaded($plot->levelName);
+			});
+			// Sort plots by level
+			usort($plots, function($plot1, $plot2) {
+				return strcmp($plot1->levelName, $plot2->levelName);
+			});
+		};
+		if(empty($levelName)) {
+			$this->db->executeSelectRaw($this->sqlGetPlotsByOwner, [$owner], $func);
+		}else{
+			$this->db->executeSelectRaw($this->sqlGetPlotsByOwnerAndLevel, [$owner, $levelName], $func);
 		}
-		// Remove unloaded plots
-		$plots = array_filter($plots, function($plot) {
-			return $this->plugin->isLevelLoaded($plot->levelName);
-		});
-		// Sort plots by level
-		usort($plots, function($plot1, $plot2) {
-			return strcmp($plot1->levelName, $plot2->levelName);
-		});
 		return $plots;
 	}
 
@@ -193,21 +158,12 @@ class MySQLProvider extends DataProvider {
 	 * @return Plot|null
 	 */
 	public function getNextFreePlot(string $levelName, int $limitXZ = 0) : ?Plot {
-		$this->reconnect();
 		$i = 0;
 		for(; $limitXZ <= 0 or $i < $limitXZ; $i++) {
-			$stmt = $this->sqlGetExistingXZ;
-			$stmt->bind_param('siiii', $levelName, $i, $i, $i, $i);
-			$result = $stmt->execute();
-			if($result === false) {
-				$this->plugin->getLogger()->error($stmt->error);
-				continue;
-			}
-			$result = $stmt->get_result();
 			$plots = [];
-			while($val = $result->fetch_array(MYSQLI_NUM)) {
+			$this->db->executeSelectRaw($this->sqlGetExistingXZ, [$levelName, $i, $i, $i, $i], function($val, $columns) use (&$plots) {
 				$plots[$val[0]][$val[1]] = true;
-			}
+			});
 			if(count($plots) === max(1, 8 * $i)) {
 				continue;
 			}
@@ -236,49 +192,7 @@ class MySQLProvider extends DataProvider {
 	}
 
 	public function close() : void {
-		if($this->db->close())
-			$this->plugin->getLogger()->debug("MySQL database closed!");
-	}
-
-	/**
-	 * @return bool
-	 */
-	private function reconnect() : bool {
-		if(!$this->db->ping()) {
-			$this->plugin->getLogger()->error("The MySQL server can not be reached! Trying to reconnect!");
-			$this->close();
-			$this->db->connect($this->settings['Host'], $this->settings['Username'], $this->settings['Password'], $this->settings['DatabaseName'], $this->settings['Port']);
-			$this->prepare();
-			if($this->db->ping()) {
-				$this->plugin->getLogger()->notice("The MySQL connection has been re-established!");
-				return true;
-			}else{
-				$this->plugin->getLogger()->critical("The MySQL connection could not be re-established!");
-				$this->plugin->getLogger()->critical("Closing level to prevent griefing!");
-				foreach($this->plugin->getPlotLevels() as $levelName => $settings) {
-					$level = $this->plugin->getServer()->getLevelByName($levelName);
-					$level->save(); // don't force in case owner doesn't want it saved
-					Server::getInstance()->unloadLevel($level, true); // force unload to prevent possible griefing
-				}
-				if($this->db->connect_error)
-					$this->plugin->getLogger()->critical("Failed to connect to the MySQL database: " . $this->db->connect_error);
-				if($this->plugin->getConfig()->getNested("MySQLSettings.ShutdownOnFailure", false)) {
-					$this->plugin->getServer()->shutdown();
-				}
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private function prepare() : void {
-		$this->sqlGetPlot = $this->db->prepare("SELECT id, name, owner, helpers, denied, biome, pvp FROM plots WHERE level = ? AND X = ? AND Z = ?;");
-		$this->sqlSavePlot = $this->db->prepare("INSERT INTO plots (`id`, `level`, `X`, `Z`, `name`, `owner`, `helpers`, `denied`, `biome`, `pvp`) VALUES((SELECT id FROM plots p WHERE p.level = ? AND X = ? AND Z = ?),?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE name = VALUES(name), owner = VALUES(owner), helpers = VALUES(helpers), denied = VALUES(denied), biome = VALUES(biome), pvp = VALUES(pvp);");
-		$this->sqlSavePlotById = $this->db->prepare("UPDATE plots SET id = ?, level = ?, X = ?, Z = ?, name = ?, owner = ?, helpers = ?, denied = ?, biome = ?, pvp = ? WHERE id = VALUES(id);");
-		$this->sqlRemovePlotById = $this->db->prepare("DELETE FROM plots WHERE id = ?;");
-		$this->sqlRemovePlot = $this->db->prepare("DELETE FROM plots WHERE level = ? AND X = ? AND Z = ?;");
-		$this->sqlGetPlotsByOwner = $this->db->prepare("SELECT * FROM plots WHERE owner = ?;");
-		$this->sqlGetPlotsByOwnerAndLevel = $this->db->prepare("SELECT * FROM plots WHERE owner = ? AND level = ?;");
-		$this->sqlGetExistingXZ = $this->db->prepare("SELECT X, Z FROM plots WHERE (level = ? AND ((abs(X) = ? AND abs(Z) <= ?) OR (abs(Z) = ? AND abs(X) <= ?)));");
+		$this->db->close();
+		$this->plugin->getLogger()->debug("MySQL database closed!");
 	}
 }
--- src/MyPlot/provider/SQLiteDataProvider.php
+++ src/MyPlot/provider/SQLiteDataProvider.php
@@ -4,27 +4,36 @@ namespace MyPlot\provider;
 
 use MyPlot\MyPlot;
 use MyPlot\Plot;
+use poggit\libasynql\libasynql;
 
 class SQLiteDataProvider extends DataProvider
 {
-	/** @var \SQLite3 $db */
+	/** @var string $db */
 	private $db;
-	/** @var \SQLite3Stmt $sqlGetPlot */
-	protected $sqlGetPlot;
-	/** @var \SQLite3Stmt $sqlSavePlot */
-	protected $sqlSavePlot;
-	/** @var \SQLite3Stmt $sqlSavePlotById */
-	protected $sqlSavePlotById;
-	/** @var \SQLite3Stmt $sqlRemovePlot */
-	protected $sqlRemovePlot;
-	/** @var \SQLite3Stmt $sqlRemovePlotById */
-	protected $sqlRemovePlotById;
-	/** @var \SQLite3Stmt $sqlGetPlotsByOwner */
-	protected $sqlGetPlotsByOwner;
-	/** @var \SQLite3Stmt $sqlGetPlotsByOwnerAndLevel */
-	protected $sqlGetPlotsByOwnerAndLevel;
-	/** @var \SQLite3Stmt $sqlGetExistingXZ */
-	protected $sqlGetExistingXZ;
+	/** @var string $sqlGetPlot */
+	protected $sqlGetPlot = "SELECT id, name, owner, helpers, denied, biome, pvp FROM plots WHERE level = :level AND X = :X AND Z = :Z;";
+	/** @var string $sqlSavePlot */
+	protected $sqlSavePlot = "INSERT OR REPLACE INTO plots (id, level, X, Z, name, owner, helpers, denied, biome, pvp) VALUES
+			((SELECT id FROM plots WHERE level = :level AND X = :X AND Z = :Z),
+			 :level, :X, :Z, :name, :owner, :helpers, :denied, :biome, :pvp);";
+	/** @var string $sqlSavePlotById */
+	protected $sqlSavePlotById = "UPDATE plots SET name = :name, owner = :owner, helpers = :helpers, denied = :denied, biome = :biome, pvp = :pvp WHERE id = :id;";
+	/** @var string $sqlRemovePlot */
+	protected $sqlRemovePlot = "DELETE FROM plots WHERE level = :level AND X = :X AND Z = :Z;";
+	/** @var string $sqlRemovePlotById */
+	protected $sqlRemovePlotById = "DELETE FROM plots WHERE id = :id;";
+	/** @var string $sqlGetPlotsByOwner */
+	protected $sqlGetPlotsByOwner = "SELECT * FROM plots WHERE owner = :owner;";
+	/** @var string $sqlGetPlotsByOwnerAndLevel */
+	protected $sqlGetPlotsByOwnerAndLevel = "SELECT * FROM plots WHERE owner = :owner AND level = :level;";
+	/** @var string $sqlGetExistingXZ */
+	protected $sqlGetExistingXZ = "SELECT X, Z FROM plots WHERE (
+				level = :level
+				AND (
+					(abs(X) = :number AND abs(Z) <= :number) OR
+					(abs(Z) = :number AND abs(X) <= :number)
+				)
+			);";
 
 	/**
 	 * SQLiteDataProvider constructor.
@@ -34,31 +43,19 @@ class SQLiteDataProvider extends DataProvider
 	 */
 	public function __construct(MyPlot $plugin, int $cacheSize = 0) {
 		parent::__construct($plugin, $cacheSize);
-		$this->db = new \SQLite3($this->plugin->getDataFolder() . "plots.db");
-		$this->db->exec("CREATE TABLE IF NOT EXISTS plots
+		$this->db = libasynql::create($plugin, [
+			"type" => "sqlite3",
+			"sqlite" => [
+				"file" => "plots.db"
+			],
+			"worker-limit" => 2
+		], [
+			"sqlite3" => []
+		]);
+		$this->db->executeGenericRaw("CREATE TABLE IF NOT EXISTS plots
 			(id INTEGER PRIMARY KEY AUTOINCREMENT, level TEXT, X INTEGER, Z INTEGER, name TEXT,
 			 owner TEXT, helpers TEXT, denied TEXT, biome TEXT, pvp INTEGER);");
-		try{
-			$this->db->exec("ALTER TABLE plots ADD pvp INTEGER;");
-		}catch(\Exception $e) {
-			// nothing :P
-		}
-		$this->sqlGetPlot = $this->db->prepare("SELECT id, name, owner, helpers, denied, biome, pvp FROM plots WHERE level = :level AND X = :X AND Z = :Z;");
-		$this->sqlSavePlot = $this->db->prepare("INSERT OR REPLACE INTO plots (id, level, X, Z, name, owner, helpers, denied, biome, pvp) VALUES
-			((SELECT id FROM plots WHERE level = :level AND X = :X AND Z = :Z),
-			 :level, :X, :Z, :name, :owner, :helpers, :denied, :biome, :pvp);");
-		$this->sqlSavePlotById = $this->db->prepare("UPDATE plots SET name = :name, owner = :owner, helpers = :helpers, denied = :denied, biome = :biome, pvp = :pvp WHERE id = :id;");
-		$this->sqlRemovePlot = $this->db->prepare("DELETE FROM plots WHERE level = :level AND X = :X AND Z = :Z;");
-		$this->sqlRemovePlotById = $this->db->prepare("DELETE FROM plots WHERE id = :id;");
-		$this->sqlGetPlotsByOwner = $this->db->prepare("SELECT * FROM plots WHERE owner = :owner;");
-		$this->sqlGetPlotsByOwnerAndLevel = $this->db->prepare("SELECT * FROM plots WHERE owner = :owner AND level = :level;");
-		$this->sqlGetExistingXZ = $this->db->prepare("SELECT X, Z FROM plots WHERE (
-				level = :level
-				AND (
-					(abs(X) = :number AND abs(Z) <= :number) OR
-					(abs(Z) = :number AND abs(X) <= :number)
-				)
-			);");
+		$this->db->executeGenericRaw("ALTER TABLE plots ADD pvp INTEGER;");
 		$this->plugin->getLogger()->debug("SQLite data provider registered");
 	}
 
@@ -68,27 +65,12 @@ class SQLiteDataProvider extends DataProvider
 	 * @return bool
 	 */
 	public function savePlot(Plot $plot) : bool {
-		$helpers = implode(",", $plot->helpers);
-		$denied = implode(",", $plot->denied);
+		$helpers = implode(',', $plot->helpers);
+		$denied = implode(',', $plot->denied);
 		if($plot->id >= 0) {
-			$stmt = $this->sqlSavePlotById;
-			$stmt->bindValue(":id", $plot->id, SQLITE3_INTEGER);
+			$this->db->executeChangeRaw($this->sqlSavePlotById, [":id" => $plot->id, ":level" => $plot->levelName, ":X" => $plot->X, ":Z" => $plot->Z, ":name" => $plot->name, ":owner" => $plot->owner, ":helpers" => $helpers, ":denied" => $denied, ":biome" => $plot->biome, ":pvp" => $plot->pvp]);
 		}else{
-			$stmt = $this->sqlSavePlot;
-			$stmt->bindValue(":level", $plot->levelName, SQLITE3_TEXT);
-			$stmt->bindValue(":X", $plot->X, SQLITE3_INTEGER);
-			$stmt->bindValue(":Z", $plot->Z, SQLITE3_INTEGER);
-		}
-		$stmt->bindValue(":name", $plot->name, SQLITE3_TEXT);
-		$stmt->bindValue(":owner", $plot->owner, SQLITE3_TEXT);
-		$stmt->bindValue(":helpers", $helpers, SQLITE3_TEXT);
-		$stmt->bindValue(":denied", $denied, SQLITE3_TEXT);
-		$stmt->bindValue(":biome", $plot->biome, SQLITE3_TEXT);
-		$stmt->bindValue(":pvp", $plot->pvp, SQLITE3_INTEGER);
-		$stmt->reset();
-		$result = $stmt->execute();
-		if(!$result instanceof \SQLite3Result) {
-			return false;
+			$this->db->executeInsertRaw($this->sqlSavePlot, [":level" => $plot->levelName, ":X" => $plot->X, ":Z" => $plot->Z, ":name" => $plot->name, ":owner" => $plot->owner, ":helpers" => $helpers, ":denied" => $denied, ":biome" => $plot->biome, ":pvp" => $plot->pvp]);
 		}
 		$this->cachePlot($plot);
 		return true;
@@ -101,18 +83,9 @@ class SQLiteDataProvider extends DataProvider
 	 */
 	public function deletePlot(Plot $plot) : bool {
 		if($plot->id >= 0) {
-			$stmt = $this->sqlRemovePlotById;
-			$stmt->bindValue(":id", $plot->id, SQLITE3_INTEGER);
+			$this->db->executeChangeRaw($this->sqlRemovePlot, [":id" => $plot->id]);
 		}else{
-			$stmt = $this->sqlRemovePlot;
-			$stmt->bindValue(":level", $plot->levelName, SQLITE3_TEXT);
-			$stmt->bindValue(":X", $plot->X, SQLITE3_INTEGER);
-			$stmt->bindValue(":Z", $plot->Z, SQLITE3_INTEGER);
-		}
-		$stmt->reset();
-		$result = $stmt->execute();
-		if(!$result instanceof \SQLite3Result) {
-			return false;
+			$this->db->executeChangeRaw($this->sqlRemovePlotById, [":level" => $plot->levelName, ":X" => $plot->X, ":Z" => $plot->Z]);
 		}
 		$plot = new Plot($plot->levelName, $plot->X, $plot->Z);
 		$this->cachePlot($plot);
@@ -127,30 +100,25 @@ class SQLiteDataProvider extends DataProvider
 	 * @return Plot
 	 */
 	public function getPlot(string $levelName, int $X, int $Z) : Plot {
-		if(($plot = $this->getPlotFromCache($levelName, $X, $Z)) !== null) {
+		if(($plot = $this->getPlotFromCache($levelName, $X, $Z)) != null) {
 			return $plot;
 		}
-		$this->sqlGetPlot->bindValue(":level", $levelName, SQLITE3_TEXT);
-		$this->sqlGetPlot->bindValue(":X", $X, SQLITE3_INTEGER);
-		$this->sqlGetPlot->bindValue(":Z", $Z, SQLITE3_INTEGER);
-		$this->sqlGetPlot->reset();
-		$result = $this->sqlGetPlot->execute();
-		if($val = $result->fetchArray(SQLITE3_ASSOC)) {
-			if($val["helpers"] === null or $val["helpers"] === "") {
+		$this->db->executeSelectRaw($this->sqlGetPlot, [":level" => $levelName, ":X" => $X, ":Z" => $Z], function($val, $columns) use ($levelName, $X, $Z, &$plot){
+			if(empty($val["helpers"])) {
 				$helpers = [];
 			}else{
 				$helpers = explode(",", (string) $val["helpers"]);
 			}
-			if($val["denied"] === null or $val["denied"] === "") {
+			if(empty($val["denied"])) {
 				$denied = [];
 			}else{
 				$denied = explode(",", (string) $val["denied"]);
 			}
 			$pvp = is_numeric($val["pvp"]) ? (bool)$val["pvp"] : null;
 			$plot = new Plot($levelName, $X, $Z, (string) $val["name"], (string) $val["owner"], $helpers, $denied, (string) $val["biome"], $pvp, (int) $val["id"]);
-		}else{
-			$plot = new Plot($levelName, $X, $Z);
-		}
+			$this->cachePlot($plot);
+		});
+		$plot = new Plot($levelName, $X, $Z);
 		$this->cachePlot($plot);
 		return $plot;
 	}
@@ -162,30 +130,26 @@ class SQLiteDataProvider extends DataProvider
 	 * @return array
 	 */
 	public function getPlotsByOwner(string $owner, string $levelName = "") : array {
-		if($levelName === "") {
-			$stmt = $this->sqlGetPlotsByOwner;
-		}else{
-			$stmt = $this->sqlGetPlotsByOwnerAndLevel;
-			$stmt->bindValue(":level", $levelName, SQLITE3_TEXT);
-		}
-		$stmt->bindValue(":owner", $owner, SQLITE3_TEXT);
 		$plots = [];
-		$stmt->reset();
-		$result = $stmt->execute();
-		while($val = $result->fetchArray(SQLITE3_ASSOC)) {
+		$func = function($val, $columns) use (&$plots) {
 			$helpers = explode(",", (string) $val["helpers"]);
 			$denied = explode(",", (string) $val["denied"]);
 			$pvp = is_numeric($val["pvp"]) ? (bool)$val["pvp"] : null;
 			$plots[] = new Plot((string) $val["level"], (int) $val["X"], (int) $val["Z"], (string) $val["name"], (string) $val["owner"], $helpers, $denied, (string) $val["biome"], $pvp, (int) $val["id"]);
+
+			$plots = array_filter($plots, function($plot) {
+				return $this->plugin->isLevelLoaded($plot->levelName);
+			});
+			// Sort plots by level
+			usort($plots, function($plot1, $plot2) {
+				return strcmp($plot1->levelName, $plot2->levelName);
+			});
+		};
+		if(empty($levelName)) {
+			$this->db->executeSelectRaw($this->sqlGetPlotsByOwner, [":owner" => $owner], $func);
+		}else{
+			$this->db->executeSelectRaw($this->sqlGetPlotsByOwnerAndLevel, [":owner" => $owner, ":level" => $levelName], $func);
 		}
-		// Remove unloaded plots
-		$plots = array_filter($plots, function($plot) {
-			return $this->plugin->isLevelLoaded($plot->levelName);
-		});
-		// Sort plots by level
-		usort($plots, function($plot1, $plot2) {
-			return strcmp($plot1->levelName, $plot2->levelName);
-		});
 		return $plots;
 	}
 
@@ -196,16 +160,11 @@ class SQLiteDataProvider extends DataProvider
 	 * @return Plot|null
 	 */
 	public function getNextFreePlot(string $levelName, int $limitXZ = 0) : ?Plot {
-		$this->sqlGetExistingXZ->bindValue(":level", $levelName, SQLITE3_TEXT);
-		$i = 0;
-		$this->sqlGetExistingXZ->bindParam(":number", $i, SQLITE3_INTEGER);
-		for(; $limitXZ <= 0 or $i < $limitXZ; $i++) {
-			$this->sqlGetExistingXZ->reset();
-			$result = $this->sqlGetExistingXZ->execute();
+		for($i = 0; $limitXZ <= 0 or $i < $limitXZ; $i++) {
 			$plots = [];
-			while($val = $result->fetchArray(SQLITE3_NUM)) {
+			$this->db->executeSelectRaw($this->sqlGetExistingXZ, [":level" => $levelName, ":number" => $i], function($val, $columns) use (&$plots) {
 				$plots[$val[0]][$val[1]] = true;
-			}
+			});
 			if(count($plots) === max(1, 8 * $i)) {
 				continue;
 			}
